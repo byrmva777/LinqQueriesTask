2b9b97a9717a545fbf45cace6c9f225b8c3c2a34
user <user@WINDOWS-OQKHAJ5>
2024-07-27 09:06:02 +0400

    Add project files.

---
+++ ConsoleApp12/Debtor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+
+public class Debtor
+{
+    public string Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public int Debt { get; set; }
+
+    public Debtor(string name, DateTime dob, string phone, string email, string address, int debt)
+    {
+        Name = name;
+        DateOfBirth = dob;
+        PhoneNumber = phone;
+        Email = email;
+        Address = address;
+        Debt = debt;
+    }
+
+
+
+}
